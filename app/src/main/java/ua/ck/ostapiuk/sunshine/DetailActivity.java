dc5a8238e77e0797cc2282e2ceaa036b2d51cc4b
vova <vova.ostapiuk@gmail.com>
2015-03-09 12:14:49 +0200

    Lesson 3 done

--- app/src/main/java/ua/ck/ostapiuk/sunshine/DetailActivity.java
+++ app/src/main/java/ua/ck/ostapiuk/sunshine/DetailActivity.java
@@ -1,17 +1,19 @@
 package ua.ck.ostapiuk.sunshine;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.TextView;
 
 
@@ -23,7 +25,7 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new DetailFragment())
                 .commit();
         }
     }
@@ -56,25 +58,55 @@ public class DetailActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
+        private static final String TAG = DetailFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
         private TextView mForecastText;
-        public PlaceholderFragment() {
+        private String mForecastStr;
+
+        public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            return rootView;
+            return inflater.inflate(R.layout.fragment_detail, container, false);
         }
 
         @Override
         public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
-            mForecastText = (TextView)view.findViewById(R.id.forecast);
             Intent intent = getActivity().getIntent();
-            if ((intent!=null)&&(intent.hasExtra(EXTRA_KEY)))
-            mForecastText.setText(intent.getStringExtra(EXTRA_KEY));
+            if ((intent != null) && (intent.hasExtra(Intent.EXTRA_TEXT)))
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) view.findViewById(R.id.detail_text))
+                    .setText(mForecastStr);
+
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            inflater.inflate(R.menu.detailfragment, menu);
+            MenuItem item = menu.findItem(R.id.action_share);
+            ShareActionProvider actionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if (actionProvider != null) {
+                actionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.i(TAG, "Share Prov is null?");
+            }
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    mForecastStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+        }
+
     }
 }
--- app/src/main/java/ua/ck/ostapiuk/sunshine/ForecastFragment.java
+++ app/src/main/java/ua/ck/ostapiuk/sunshine/ForecastFragment.java
@@ -18,7 +18,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,19 +28,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-
-import ua.ck.ostapiuk.sunshine.R;
 
 public class ForecastFragment extends Fragment {
-    private ArrayAdapter<String> adapter;
     private static final String TAG = ForecastFragment.class.getName();
+    private ArrayAdapter<String> adapter;
     public ForecastFragment() {
     }
 
@@ -73,17 +68,105 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getActivity(),DetailActivity.class);
-                intent.putExtra("Forecast",adapter.getItem(position));
+                intent.putExtra(Intent.EXTRA_TEXT, adapter.getItem(position));
                 startActivity(intent);
             }
         });
         return rootView;
     }
-    public class FetchWeatherTask extends AsyncTask<String,Void,String[]>{
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            updateWeather();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private String getReadableDateString(long time) {
+        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+        return shortenedDateFormat.format(time);
+        }
+
+        private String formatHighLows(double high, double low,String unitType){
+            if (unitType.equals(getString(R.string.pref_units_imperial))) {
+                high = (high * 1.8) + 32;
+                low = (low * 1.8) + 32;
+            } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
+                Log.d(TAG, "Unit type not found: " + unitType);
+            }
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            return roundedHigh + "/" + roundedLow;
+        }
+
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays) throws JSONException {
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.optJSONArray(OWM_LIST);
+            Time dayTime = new Time();
+            dayTime.setToNow();
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+            dayTime = new Time();
+            String[] resultStrs = new String[numDays];
+            SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(
+                getString(R.string.pref_units_key),
+                getString(R.string.pref_units_metric));
+            for(int i = 0; i < weatherArray.length(); i++) {
+                String day;
+                String description;
+                String highAndLow;
+                JSONObject dayForecast = weatherArray.optJSONObject(i);
+                long dateTime;
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+                JSONObject weatherObject = dayForecast.optJSONArray(OWM_WEATHER).optJSONObject(0);
+                description = weatherObject.optString(OWM_DESCRIPTION);
+                JSONObject temperatureObject = dayForecast.optJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.optDouble(OWM_MAX);
+                double low = temperatureObject.optDouble(OWM_MIN);
+                highAndLow = formatHighLows(high, low,unitType);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+
+
+            }
+            return resultStrs;
+        }
+
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = preferences.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        weatherTask.execute(location);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
         private final String TAG = FetchWeatherTask.class.getName();
+
         @Override
         protected String[] doInBackground(String... params) {
-            if (params.length == 0){
+            if (params.length == 0) {
                 return null;
             }
             HttpURLConnection httpURLConnection = null;
@@ -100,13 +183,13 @@ public class ForecastFragment extends Fragment {
                 final String UNITS_PARAM = "units";
                 final String DAYS_PARAM = "cnt";
                 Uri uri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM,params[0])
-                        .appendQueryParameter(FORMAT_PARAM,format)
-                        .appendQueryParameter(UNITS_PARAM,units)
-                        .appendQueryParameter(DAYS_PARAM,Integer.toString(numDays))
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, units)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
                         .build();
                 URL url = new URL(uri.toString());
-                Log.i(TAG,uri.toString());
+                Log.i(TAG, uri.toString());
                 httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("GET");
                 httpURLConnection.connect();
@@ -147,98 +230,15 @@ public class ForecastFragment extends Fragment {
 
         @Override
         protected void onPostExecute(String[] strings) {
-            if (strings!=null) {
+            if (strings != null) {
                 adapter.clear();
 
-           for (String string : strings){
-               adapter.add(string);
-           }
-            adapter.notifyDataSetChanged();
-        }
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        updateWeather();
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-       inflater.inflate(R.menu.forecastfragment, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_refresh){
-            updateWeather();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-        private String getReadableDateString(long time){
-            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
-            return shortenedDateFormat.format(time);
-        }
-        private String formatHighLows(double high, double low,String unitType){
-            if (unitType.equals(getString(R.string.pref_units_imperial))) {
-                high = (high * 1.8) + 32;
-                low = (low * 1.8) + 32;
-            } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
-                Log.d(TAG, "Unit type not found: " + unitType);
+                for (String string : strings) {
+                    adapter.add(string);
+                }
+                adapter.notifyDataSetChanged();
             }
-            long roundedHigh = Math.round(high);
-            long roundedLow = Math.round(low);
-
-            return roundedHigh + "/" + roundedLow;
         }
-        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays) throws JSONException {
-            final String OWM_LIST = "list";
-            final String OWM_WEATHER = "weather";
-            final String OWM_TEMPERATURE = "temp";
-            final String OWM_MAX = "max";
-            final String OWM_MIN = "min";
-            final String OWM_DESCRIPTION = "main";
-            JSONObject forecastJson = new JSONObject(forecastJsonStr);
-            JSONArray weatherArray = forecastJson.optJSONArray(OWM_LIST);
-            Time dayTime = new Time();
-            dayTime.setToNow();
-            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
-            dayTime = new Time();
-            String[] resultStrs = new String[numDays];
-            SharedPreferences sharedPrefs =
-                PreferenceManager.getDefaultSharedPreferences(getActivity());
-            String unitType = sharedPrefs.getString(
-                getString(R.string.pref_units_key),
-                getString(R.string.pref_units_metric));
-            for(int i = 0; i < weatherArray.length(); i++) {
-                String day;
-                String description;
-                String highAndLow;
-                JSONObject dayForecast = weatherArray.optJSONObject(i);
-                long dateTime;
-                dateTime = dayTime.setJulianDay(julianStartDay+i);
-                day = getReadableDateString(dateTime);
-                JSONObject weatherObject = dayForecast.optJSONArray(OWM_WEATHER).optJSONObject(0);
-                description = weatherObject.optString(OWM_DESCRIPTION);
-                JSONObject temperatureObject = dayForecast.optJSONObject(OWM_TEMPERATURE);
-                double high = temperatureObject.optDouble(OWM_MAX);
-                double low = temperatureObject.optDouble(OWM_MIN);
-                highAndLow = formatHighLows(high, low,unitType);
-                resultStrs[i] = day + " - " + description + " - " + highAndLow;
-
-
-            }
-            return resultStrs;
-        }
-    private void updateWeather(){
-        FetchWeatherTask weatherTask = new FetchWeatherTask();
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String location = preferences.getString(getString(R.string.pref_location_key),
-            getString(R.string.pref_location_default));
-        weatherTask.execute(location);
     }
     }
 
